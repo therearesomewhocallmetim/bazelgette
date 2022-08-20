8551962a7b651332cf36fd9258d5bc185b36b6f0
Timofey Danshin <t.danshin@gmail.com>
2022-08-20 18:54:31 +0300

    Use library from the binary

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 import requests
+from libs import main
+
 
 if __name__ == "__main__":
-    print("hello")
+    print(main.get_greeting())
     print(sys.version)
     print(requests.get("https://example.com").content)
